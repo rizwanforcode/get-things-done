9a45b5f2352b56c9e03982fb046018b500fad8b6
Rizwan Ahmad <rizwanforcode@gmail.com>
2022-11-03 19:43:50 +0545

    Added the entire markup in appjs and changed the root react element from div root to body with id body

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const body = ReactDOM.createRoot(document.getElementById("body"));
+body.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
