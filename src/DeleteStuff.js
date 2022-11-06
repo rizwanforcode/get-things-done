32dc883aa910a1d2de403134de3954c52b2b4e78
Rizwan Ahmad <rizwanforcode@gmail.com>
2022-11-06 19:30:32 +0545

    Implemented the dirtiest remove list function

--- src/DeleteStuff.js
+++ src/DeleteStuff.js
@@ -7,7 +7,28 @@ const DeleteStuff = ({ lists, setLists, todos, setTodos }) => {
   };
 
   const deleteList = () => {
-    setLists(lists.filter((list) => !list.active));
+    if (lists.length === 1) {
+      return;
+    }
+    const activeList = lists.find((list) => list.active);
+    const activeListId = activeList.id;
+
+    const sortedList = lists.sort(
+      (a, b) => Number(a.datetime) - Number(b.datetime)
+    );
+
+    const activeListIndex = sortedList.indexOf(activeList);
+
+    setLists(
+      sortedList.map((list, index) => ({
+        ...list,
+        active: index === (activeListIndex + 1) % sortedList.length,
+      }))
+    );
+
+    setLists((prevLists) => {
+      return prevLists.filter((list) => list.id !== activeListId);
+    });
   };
   return (
     <div className="delete-stuff">
